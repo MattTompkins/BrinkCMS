0c8cdea6559492b37ada7875d3843affb11f1d71
MattTompkins <51715889+MattTompkins@users.noreply.github.com>
2023-01-28 14:53:26 +0000

    Add post migration

---
+++ BrinkCMS/database/migrations/2023_01_28_144152_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->longText('content');
+            $table->string('thumbnail')->nullable();
+            $table->integer('author');
+            $table->string('categories')->nullable();
+            $table->string('tags')->nullable();
+            $table->string('status')->default('unpublished');
+            $table->dateTime('publishedAt')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
--- BrinkCMS/routes/web.php
+++ BrinkCMS/routes/web.php
@@ -3,29 +3,26 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/** Homepage */
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+/** Frontend Routes */
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/** Auth Routes */
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
+/** Backend Routes */
+
+
+require __DIR__ . '/auth.php';
