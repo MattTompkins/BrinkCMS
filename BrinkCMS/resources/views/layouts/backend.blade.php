43d09ca6c750421c96721cfd5af2776d3648d7c5
MattTompkins <51715889+MattTompkins@users.noreply.github.com>
2023-01-29 16:15:56 +0000

    Support custom footer scripts

--- BrinkCMS/resources/views/layouts/backend.blade.php
+++ BrinkCMS/resources/views/layouts/backend.blade.php
@@ -4,15 +4,14 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+
+        <title>Dashboard</title>
         <link href="css/styles.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}" />
-    @yield('headerStyles')
-    @yield('headerScripts')
+
+        @yield('headerStyles')
+        @yield('headerScripts')
+        
     </head>
     <body class="sb-nav-fixed">
         @include('backend.components.topnav')
@@ -28,6 +27,8 @@
                @include('backend.components.footer')
             </div>
         </div>
+
+        @yield('footerScripts')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="{{ asset('backend/js/main.js') }}"></script>
         
